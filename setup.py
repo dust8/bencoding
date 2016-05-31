e8d509a80dbfae3f81866a40c77dc4828c9eb34c
dust8 <adbcdust@mail.com>
2016-05-31 19:41:32 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup, find_packages
 
-
-setup(
-    name='bencoding',
-    version='0.2.2',
-
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-
-    zip_safe=True,
-
-    description='bencoding is implemented over python3.',
-    url='https://github.com/dust8/bencoding',
-
-    keywords='bencode bencoding',
-)
+setup(name='bencoding',
+      version='0.2.4',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      zip_safe=True,
+      description='bencoding is implemented over python3.',
+      url='https://github.com/dust8/bencoding',
+      keywords='bencode bencoding torrent bittorrent', )
--- src/bencoding/bencoding.py
+++ src/bencoding/bencoding.py
@@ -25,22 +25,19 @@ def bencode(x):
     b'd4:spaml1:a1:bee'
     '''
     if isinstance(x, str):
-        try:
-            data = x.encode('latin1')
-        except:
-            data = x.encode()
-        return str(len(data)).encode('latin1')+b':'+data
+        data = x.encode()
+        return str(len(data)).encode() + b':' + data
     elif isinstance(x, bytes):
-        return str(len(x)).encode('latin1')+b':'+x
+        return str(len(x)).encode() + b':' + x
     elif isinstance(x, (int, float)):
-        return b'i'+str(x).encode('latin1')+b'e'
+        return b'i' + str(x).encode() + b'e'
     elif isinstance(x, list):
         data = b''.join([bencode(i) for i in x])
-        return b'l'+data+b'e'
+        return b'l' + data + b'e'
     elif isinstance(x, dict):
         items = sorted(list(x.items()))
         data = b''.join([bencode(k) + bencode(v) for k, v in items])
-        return b'd'+data+b'e'
+        return b'd' + data + b'e'
     else:
         raise BencodingError('bencode error')
 
@@ -67,35 +64,37 @@ def bdecode(x):
     return r
 
 
+int_list = [ord(str(i)) for i in range(10)]
+
+
 def _bdecode(x, f=0):
     r = x[f]
-    int_list = [ord(str(i)) for i in range(10)]
 
     if r in int_list:
         colon = x.index(b':', f)
         n = int(x[f:colon])
         colon += 1
         try:
-            return x[colon:colon+n].decode(), colon+n
+            return x[colon:colon + n].decode(), colon + n
         except:
-            return x[colon:colon+n], colon+n
+            return x[colon:colon + n], colon + n
     elif r == ord(b'i'):
         f += 1
         newf = x.index(b'e', f)
         n = int(x[f:newf])
-        return n, newf+1
+        return n, newf + 1
     elif r == ord(b'l'):
-        r, f = [], f+1
+        r, f = [], f + 1
         while x[f] != ord(b'e'):
             v, f = _bdecode(x, f)
             r.append(v)
-        return r, f+1
+        return r, f + 1
     elif r == ord(b'd'):
-        r, f = {}, f+1
+        r, f = {}, f + 1
         while x[f] != ord(b'e'):
             k, f = _bdecode(x, f)
             r[k], f = _bdecode(x, f)
-        return r, f+1
+        return r, f + 1
 
 
 if __name__ == '__main__':
